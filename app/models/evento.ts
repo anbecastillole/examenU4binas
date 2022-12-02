74c9310cf6816aad719c12d46604294dfb56ac8f
Ana Cast <anbecastillole@ittepic.edu.mx>
2022-12-02 09:15:11 -0700

    ana bertha castillo leal

---
+++ app/models/evento.ts
@@ -0,0 +1,8 @@
+export class Evento {
+    fecha!: string;
+    total!: number;
+    cliente!: string;
+    telefono!: string;
+    
+    
+}
---
+++ app/services/evento.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Evento } from '../models/evento';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventoService {
+  private eventos : Evento[];
+  constructor(private firestore:AngularFirestore) {
+    this.eventos= [
+      {
+        fecha: "05/12/2022",
+        total: 1000,
+        cliente: "ana castillo",
+        telefono: "3111073571"
+      },
+    ]
+   }
+
+  public addEvento(evento: Evento){ //para que agregue el evento desde el cliente
+    return this.firestore.collection('evento').add(evento);
+  }
+
+  public getEventos(): Observable<Evento[]>{ //que muestre todos los eventos desde admin
+    return this.firestore.collection('evento').snapshotChanges().pipe(
+      map(actions =>{
+        return actions.map(a=>{
+          //console.log(a);
+          const data = a.payload.doc.data() as Evento;
+          //console.log(data);
+          const id = a.payload.doc.id;
+          return {id,...data};
+        });
+      })
+    );
+  }
+
+  public getEventoById(id: string){ //ver evento, si es necesario
+    let result= this.firestore.collection('evento').doc(id).valueChanges();
+    return result;
+  }
+
+  public getClientByTelefono(tel: string){ //para consultar desde el login si existe el cliente, si sí lo lleve a su pagina
+    let result= this.firestore.collection('cliente').doc(tel).valueChanges();
+    return result;
+  }
+
+
+
+}
+
+
+
